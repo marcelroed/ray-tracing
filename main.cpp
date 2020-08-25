b478ceb2157a61dc95cddd41a4097aa2674c3d44
marcelroed <marcelroed@gmail.com>
2020-08-25 14:39:19 +0200

    Change materials

--- main.cpp
+++ main.cpp
@@ -41,10 +41,10 @@ int main(int argc, char** argv) {
     // World
     HittableList world;
 
-    auto material_ground = make_shared<Lambertian>(color(81./255, 76./255, 91./255));
+    auto material_ground = make_shared<Metal>(color(81./255, 76./255, 91./255), 0.00002);
     auto material_center = make_shared<Lambertian>(color(.7, .3, .3));
-    auto material_left = make_shared<Metal>(color(0.8, 0.8, 0.8), 0.3);
-    auto material_right = make_shared<Metal>(color(0.8, 0.6, 0.2), 1.0);
+    auto material_left = make_shared<Metal>(color(0.8, 0.8, 0.8), 0);
+    auto material_right = make_shared<Metal>(color(0.8, 0.6, 0.2), 0);
 
     world.add(make_shared<Sphere>(point3(0, -100.5, -1), 100, material_ground));
     world.add(make_shared<Sphere>(point3(0, 0, -1), 0.5, material_center));
